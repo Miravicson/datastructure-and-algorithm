d861d76a6858989c10a077657c5e81db08c74467
Miravicson <victorughonu@gmail.com>
2025-05-22 12:36:13 +0100

    feat: Implement coinChange function to calculate the fewest coins needed for a given amount

---
+++ src/coinChange.ts
@@ -0,0 +1,46 @@
+/**
+ * You are given coins of different values and a total amount of money.
+ * write a function to compute the fewest number of coins that you need to make up that amount.
+ * If it’s not possible, return -1.
+ */
+
+function coinChange(
+  amount: number,
+  coins: number[],
+  memo: Record<number, number> = {},
+): number {
+  if (amount in memo) {
+    return memo[amount];
+  }
+  if (amount === 0) {
+    return 0;
+  }
+
+  if (amount < 0) return -1;
+
+  let shortestCombination = -1;
+
+  for (let coin of coins) {
+    const resultCombo = coinChange(amount - coin, coins, memo);
+
+    if (resultCombo >= 0) {
+      const combination = resultCombo + 1;
+
+      if (shortestCombination === -1 || shortestCombination > combination) {
+        shortestCombination = combination;
+      }
+    }
+  }
+
+  memo[amount] = shortestCombination;
+
+  return shortestCombination;
+}
+
+console.log(coinChange(11, [1, 2, 5])); // 3
+console.log(coinChange(3, [2])); // -1
+console.log(coinChange(0, [1])); // 0
+console.log(coinChange(2, [1])); // 2
+console.log(coinChange(6249, [186, 419, 83, 408])); // 20
+console.log(coinChange(27, [2, 5, 10, 1])); // 4
+console.log(coinChange(8839, [3, 7, 405, 436])); // 25
---
+++ src/twoSums.ts
@@ -0,0 +1,7 @@
+/**
+ * Given an array of integers nums and an integer target,
+return the indices of the two numbers such that they add up to the target.
+	•	You may assume that exactly one solution exists.
+	•	You may not use the same element twice.
+	•	Return the indices in any order.
+ */
